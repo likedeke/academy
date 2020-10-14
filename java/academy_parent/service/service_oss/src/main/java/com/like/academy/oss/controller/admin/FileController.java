084e0d6d80854473c9c8c7665860b66cb6ae02ea
like <980650920@qq.com>
2020-10-14 18:41:30 +0800

    1
    1

--- java/academy_parent/service/service_oss/src/main/java/com/like/academy/oss/controller/admin/FileController.java
+++ java/academy_parent/service/service_oss/src/main/java/com/like/academy/oss/controller/admin/FileController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author likeLove
@@ -51,11 +52,11 @@ public class FileController {
     @ApiOperation ("文件删除")
     @DeleteMapping ("/del")
     public Result del() {
-       /* try {
+        try {
             TimeUnit.SECONDS.sleep(3);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }*/
+        }
         return Result.ok().message("文件删除成功" + "port：" + serverPort);
     }
 }
